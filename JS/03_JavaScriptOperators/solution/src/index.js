86474ba7a49f99fa478fb8a83a199f984ca36973
Martin Nolan <martinnolan_1@live.co.uk>
2023-08-24 10:13:40 +0100

    task 3

--- JS/03_JavaScriptOperators/solution/src/index.js
+++ JS/03_JavaScriptOperators/solution/src/index.js
@@ -1,26 +1,26 @@
 // Aritmetic Operators
-// console.log(5 + 5);
-// console.log(5 * 10);
-// console.log(10 % 3);
-// console.log(5 + 10 / 2 * 5 - 10);
-// console.log((6 + 10) / 2 * 5 - 10);
-
-// Assignment Operators
-// console.log(x = x + 1);
-// console.log(x+= 1);
-// console.log(x++);
-// console.log(++x);
-
-// Relational Operators
-// console.log(5 > 3);
-// console.log(3 != 3);
-// console.log(3 <= 2 && 5 >2);
-// console.log(!5>3);
-
-// Mismatched Types
-// console.log(5 + "5");
-// console.log(5 + true);
-// console.log(5 * "5");
-// console.log(1 == true);
-// console.log(1 === true);
-
+console.log(5 + 5); // Output - 10
+console.log(5 * 10); // Output - 50
+console.log(10 % 3); // Output - 1
+console.log(5 + (10 / 2) * 5 - 10); // Output - 20
+console.log(((6 + 10) / 2) * 5 - 10); // Output - 30
+console.log("---------------");
+//Assignment Operators
+x = 0;
+console.log((x = x + 1)); // Output - 1
+console.log((x += 1)); // Output - 2
+console.log(x++); // Output - 2
+console.log(++x); // Output - 4
+console.log("---------------");
+//Relational Operators
+console.log(5 > 3); // Output - true
+console.log(3 != 3); // Output - false
+console.log(3 <= 2 && 5 > 2); // Output - false
+console.log(!5 > 3); // Output - false
+console.log("---------------");
+//Mismatched Types
+console.log(5 + "5"); // Output - 55
+console.log(5 + true); // Output - 6
+console.log(5 * "5"); // Output - 25
+console.log(1 == true); // Output - true
+console.log(1 === true); // Output - false
