457f446c0033cf97375fb6262548e4c3359c4723
Martin Nolan <martinnolan_1@live.co.uk>
2023-08-25 12:42:48 +0100

    task 7

--- JS/07_JavaScriptFunctions/starter/src/index.js
+++ JS/07_JavaScriptFunctions/starter/src/index.js
@@ -1,13 +1,40 @@
 const movies = [
-    {"title": "Jaws", "director": "Steven Spielberg", "year": "1975"},
-    {"title": "Star Wars", "director": "George Lucas", "year": "1977"},
-    {"title": "Avengers: Infinity War", "director": "Anthony and Joe Russo", "year": "2018"},
-    {"title": "Top Gun", "director": "Tony Scott", "year": "1986"},
-    {"title": "Justice League", "director": "Zack Snyder", "year": "2017"}
+  { title: "Jaws", director: "Steven Spielberg", year: "1975" },
+  { title: "Star Wars", director: "George Lucas", year: "1977" },
+  {
+    title: "Avengers: Infinity War",
+    director: "Anthony and Joe Russo",
+    year: "2018",
+  },
+  { title: "Top Gun", director: "Tony Scott", year: "1986" },
+  { title: "Justice League", director: "Zack Snyder", year: "2017" },
 ];
 
 // Part 1 - Create a function to find a movie and output it's detail
-        
-        
+function FindMovie(search) {
+  for (const movie of movies) {
+    if (movie.title === search) {
+      console.log(
+        "Title: ",
+        movie.title,
+        ", Director: ",
+        movie.director,
+        ", Year: ",
+        movie.year
+      );
+    }
+  }
+}
+
+FindMovie("Jaws");
 
 // Part 2 = Create a function to return a movie object
+function FindMovieObject(search) {
+  for (const movie of movies) {
+    if (movie.title === search) {
+      console.log(movie);
+    }
+  }
+}
+
+FindMovieObject("Star Wars");
